1675b314cee5a46528e3ffd8e758a13ba7217838
K0unna00 <78pf4jz@code.edu.az>
2024-11-07 22:56:06 +0400

    Add project files.

---
+++ Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿static bool IsGoodBinaryStr(string str)
+{
+    int countOne = 0;
+    int countZero = 0;
+
+    if (str.Length % 2 != 0)
+    {
+        return false;
+    }
+
+    foreach (char ch in str)
+    {
+        if (ch == '1')
+            countOne++;
+
+        else if (ch == '0')
+            countZero++;
+
+        else
+            return false;
+
+        if (countZero > countOne)
+            return false;
+    }
+
+    return countOne == countZero;
+}
+
+static void Test()
+{
+    string[] cases = ["111000", "10010", "101"];
+
+    foreach (string str in cases)
+    {
+        Console.WriteLine($"{IsGoodBinaryStr(str)}");
+    }
+}
+
+Test();
